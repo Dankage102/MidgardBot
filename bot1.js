4f3b7bad0dce8fbdf45e255a45390dd2b9cf521a
Marek Pierzchała <markph1990@gmail.com>
2019-02-08 22:03:33 +0100

    first run of refactor

--- bot1.js
+++ bot1.js
@@ -1,97 +1,62 @@
 const Discord = require("discord.js");
 const snekfetch = require("snekfetch");
-const client = new Discord.Client(); 
+const auth = require('./auth.json');
+
 const prefix = "!";
-var auth = require('./auth.json');
-var api = [
-    "https://api.soldat.pl/v0/server/51.68.213.93/23081",           //CTF#1
-    "https://api.soldat.pl/v0/server/138.201.55.232/25660",         //CTF#2
-    "https://api.soldat.pl/v0/server/162.221.187.210/25020",        //Final Bout
-    "https://api.soldat.pl/v0/server/162.221.187.210/25000",        //HTF
-    "https://api.soldat.pl/v0/server/51.68.213.93/23080",           //Run Mode
-    "https://api.soldat.pl/v0/server/51.68.213.93/23082",           //Climb
-    "https://api.soldat.pl/v0/server/51.68.213.93/23083"            //AlphaZRPG
- ];
-var timeInterval;
-var data1;
-var data2;
-var data3;
-var data4;
-var data5;
-var data6;
-var data7;
+const api = [
+  {ip: "51.68.213.93", port: "23081"}, // CTF#1
+  {ip: "138.201.55.232", port: "25660"}, // CTF#2
+  {ip: "162.221.187.210", port: "25020"}, // Final Bout
+  {ip: "162.221.187.210", port: "25000"}, // HTF
+  {ip: "51.68.213.93", port: "23080"}, // Run Mode
+  {ip: "51.68.213.93", port: "23082"}, //Climb
+  {ip: "51.68.213.93", port: "23083"} // AlphaZRPG
+];
 
-client.on("ready", async ()=>{
-    console.log(`Bot is ready! ${client.user.username}`);
+const getApiURL = (ip, port) => `https://api.soldat.pl/v0/server/{ip}/{port}`
 
-    try {
-        let link = await client.generateInvite(["ADMINISTRATOR"]);
-        console.log(link);
-    } catch(e) {
-        console.log(e.stack);
-    }
+const client = new Discord.Client(); 
+
+client.login(auth.token);
+
+client.on("ready", ()=>{
+  console.log(`Bot is ready! ${client.user.username}`);
 });
 
 client.on("message", async message => {
+  let timeInterval;
 
-    if(message.channel.type === "dm") return;
+  if(message.channel.type === "dm") return;
 
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
+  const messageArray = message.content.split(" ");
+  const command = messageArray[0];
+  const args = messageArray.slice(1);
 
-    if(!command.startsWith(prefix)) return;
+  if(!command.startsWith(prefix)) return;
 
-    if(command === `${prefix}statusMID`){
+  if(command === `${prefix}statusMID`){
 		update();
 		clearInterval(timeInterval);
 		timeInterval = setInterval(update,60000);
-    }
+  }
 });
 
 async function status(){
-	var embed;
-	await snekfetch.get(api[0]).then(r =>{
-		let body = r.body;
-		data1=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	}).then(
-	await snekfetch.get(api[1]).then(r =>{
-		let body = r.body;
-		data2=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
-	await snekfetch.get(api[2]).then(r =>{
-		let body = r.body;
-		data3=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
-	await snekfetch.get(api[3]).then(r =>{
-		let body = r.body;
-		data4=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
-	await snekfetch.get(api[4]).then(r =>{
-		let body = r.body;
-		data5=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
-	await snekfetch.get(api[5]).then(r =>{
-		let body = r.body;
-		data6=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
-	await snekfetch.get(api[6]).then(r =>{
-		let body = r.body;
-		data7=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	}));
-	
-	embed = new Discord.RichEmbed()
-	.setAuthor(client.user.username,client.user.avatarURL)
-	.setColor(Math.floor(Math.random() * 16777214) + 1)
-	.addField(":flag_gb:**Midgard [CTF] #1**","**Address**: <soldat://51.68.213.93:23081> \n **Players:**  `" + data1[0] + "/" + data1[1] + "`<:crouch:533700465670619197> **Map:**  `" + data1[2] + "`:map:")
-	.addField(":flag_de:**Midgard [CTF] #2**","**Address**: <soldat://138.201.55.232:25660> \n **Players:**  `" + data2[0] + "/" + data2[1] + "`<:crouch:533700465670619197> **Map:**  `" + data2[2] + "`:map:")
-	.addField(":flag_us:**Midgard [Final Bout]**","**Address**: <soldat://162.221.187.210:25020/003> \n **Players:**  `" + data3[0] + "/" + data3[1] + "`<:crouch:533700465670619197> **Map:**  `" + data3[2] + "`:map:")
-	.addField(":flag_us:**Midgard [HTF]**","**Address**: <soldat://162.221.187.210:25000> \n **Players:**  `" + data4[0] + "/" + data4[1] + "`<:crouch:533700465670619197> **Map:**  `" + data4[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Run Mode]**","**Address**: <soldat://51.68.213.93:23080> \n **Players:**  `" + data5[0] + "/" + data5[1] + "`<:crouch:533700465670619197> **Map:**  `" + data5[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Climb]**","**Address**: <soldat://51.68.213.93:23082> \n **Players:**  `" + data6[0] + "/" + data6[1] + "`<:crouch:533700465670619197> **Map:**  `" + data6[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [AlphaZRPG]**","**Address**: <soldat://51.68.213.93:23083> \n **Players:**  `" + data7[0] + "/" + data7[1] + "`<:crouch:533700465670619197> **Map:**  `" + data7[2] + "`:map:")
-	.setTimestamp(new Date());
-	await client.channels.get(`537011480973934592`).send(embed);
+  const statuses = await Promise.all(api.map(({ip, port}) => {
+    return snekfetch.get(getApiURL(ip, port));
+  }))
+  
+	const embed = new Discord.RichEmbed()
+	  .setAuthor(client.user.username,client.user.avatarURL)
+	  .setColor(Math.floor(Math.random() * 16777214) + 1)
+
+  statuses.forEach(({body: {NumPlayers, MaxPlayers, CurrentMap}}, i) => {
+    embed.addField(`:flag_gb:**Midgard [CTF] #1****Address**: <soldat://{api[i].ip}:api[i].port> \n **Players:**  \`{NumPlayers}/{MaxPlayers}\`<:crouch:533700465670619197> **Map:**  \`${CurrentMap}\`:map:`)
+  })
+
+  embed.setTimestamp(new Date());
+  
+	client.channels.get(`537011480973934592`).send(embed);
 }
 
 async function update(){
@@ -99,5 +64,3 @@ async function update(){
 	client.channels.get(`537011480973934592`).bulkDelete(fetched);
 	status();
 }
-
-client.login(auth.token);
